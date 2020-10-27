689ed26f7b29848afbddb561b953c699766bceed
JayramNanoskar <jayram.n978@gmail.com>
2020-10-27 21:35:31 +0530

    using methods and constructors

--- demo2.js
+++ demo2.js
@@ -1,8 +1,12 @@
 var Person = /** @class */ (function () {
-    function Person() {
+    function Person(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
+    Person.prototype.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
     return Person;
 }());
-var aPerson = new Person(); //type declaration for custom classes same as primitive 
-aPerson.firstName = "Jayram";
-console.log(aPerson.firstName);
+var aPerson = new Person("Jayram", "Nanoskar"); //type declaration for custom classes same as primitive 
+console.log(aPerson.getFullName());
--- demo2.ts
+++ demo2.ts
@@ -1,8 +1,18 @@
 class Person{
     firstName: string;
     lastName: string;
+
+    constructor(firstName: string, lastName: string){// we cannot have overloaded constructor in ts, only one constructor is allowed
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullName(){
+        return this.firstName + " " + this.lastName;
+    }
+    
 }
 
-var aPerson: Person = new Person();//type declaration for custom classes same as primitive 
-aPerson.firstName = "Jayram";
-console.log(aPerson.firstName);
+var aPerson: Person = new Person("Jayram", "Nanoskar");//type declaration for custom classes same as primitive 
+
+console.log(aPerson.getFullName());
