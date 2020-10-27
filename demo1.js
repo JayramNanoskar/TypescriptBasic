6b368e169f2f9acc3bbbc8554cd4c7f82ff02285
JayramNanoskar <jayram.n978@gmail.com>
2020-10-27 16:33:00 +0530

    using arrays and tuples

--- demo1.js
+++ demo1.js
@@ -1,8 +1,19 @@
 var func = function () { return "ok"; };
 console.log(func());
-var a; //using primitive data type 
+var a; //using primitive data type
+var b; // postfix type declaration in ts, some other languages like java follows prefix type declaration
 a = 10;
 //a = true; 
 // shows error in ts as Type 'boolean' is not assignable to type 'number' but still after compilation .js file get generated 
 // because it works in .js as previously. Here typescipt adding development side advantages as we get to know about error at on/before compiletime
+b = true;
 console.log(a);
+console.log(b);
+var myArr; // it will store numbers value only, not flexible as js array for flexibility we can use tuple in ts
+myArr = [2, 4, 1, 9];
+myArr.pop();
+myArr.push(11);
+console.log(myArr);
+var myTuple;
+myTuple = [1, false];
+console.log(myTuple);
--- demo1.ts
+++ demo1.ts
@@ -1,11 +1,26 @@
 var func =  () =>  "ok"; 
 console.log(func());
 
-var a: number; //using primitive data type 
+var a: number; //using primitive data type
+var b: boolean; // postfix type declaration in ts, some other languages like java follows prefix type declaration
 
 a = 10;
 //a = true; 
 // shows error in ts as Type 'boolean' is not assignable to type 'number' but still after compilation .js file get generated 
 // because it works in .js as previously. Here typescipt adding development side advantages as we get to know about error at on/before compiletime
+b = true;
 
 console.log(a);
+console.log(b);
+
+var myArr: number[];// it will store numbers value only, not flexible as js array for flexibility we can use tuple in ts
+myArr = [2, 4, 1, 9];
+
+myArr.pop();
+myArr.push(11);
+console.log(myArr);
+
+var myTuple: [number, boolean];
+myTuple = [1, false];
+
+console.log(myTuple);
