0a5cc6d562a451af4356ca8868630eeb9e65ee38
JayramNanoskar <jayram.n978@gmail.com>
2020-10-27 16:10:20 +0530

    using type declaration in ts file

--- demo1.js
+++ demo1.js
@@ -1,2 +1,8 @@
 var func = function () { return "ok"; };
 console.log(func());
+var a; //using primitive data type 
+a = 10;
+//a = true; 
+// shows error in ts as Type 'boolean' is not assignable to type 'number' but still after compilation .js file get generated 
+// because it works in .js as previously. Here typescipt adding development side advantages as we get to know about error at on/before compiletime
+console.log(a);
