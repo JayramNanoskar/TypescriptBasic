49b3e66646fbd3a2b60d082d0c1a52da93e44ebb
JayramNanoskar <jayram.n978@gmail.com>
2020-10-27 21:11:14 +0530

    using classes

---
+++ demo2.js
@@ -0,0 +1,8 @@
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var aPerson = new Person(); //type declaration for custom classes same as primitive 
+aPerson.firstName = "Jayram";
+console.log(aPerson.firstName);
---
+++ demo2.ts
@@ -0,0 +1,8 @@
+class Person{
+    firstName: string;
+    lastName: string;
+}
+
+var aPerson: Person = new Person();//type declaration for custom classes same as primitive 
+aPerson.firstName = "Jayram";
+console.log(aPerson.firstName);
