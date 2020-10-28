a3f6d86433995cb406ee17205a60fee67eabd886
JayramNanoskar <jayram.n978@gmail.com>
2020-10-29 01:12:02 +0530

    using node module | lodash

--- demo11/src/index.ts
+++ demo11/src/index.ts
@@ -2,6 +2,12 @@
 //tsc --init -> to create tsconfig.json file
 
 import { Student } from "./student";
+import * as _ from "lodash";
 
 let student = new Student("Vikas", "Rane");
 console.log(student.getFullName());
+
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+_.reverse(arr);
+console.log(arr);
